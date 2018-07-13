b75151d974babc2b5889d0a8d6c4b4f2352a2cf1
Eddy <eddy@hotmail.com>
2018-07-13 11:16:50 -0500

    Update Comments and README

--- 01-ClimbingStairs/ClimbingStairs.js
+++ 01-ClimbingStairs/ClimbingStairs.js
@@ -1,5 +1,10 @@
 /*
+
+This Excercise was taken from www.codefights.com a place to learn algorithms and improve de programming skills
+Problem:
+
 You are climbing a staircase that has n steps. You can take the steps either 1 or 2 at a time. Calculate how many distinct ways you can climb to the top of the staircase.
+
 Example
 
 For n = 1, the output should be
@@ -11,7 +16,7 @@ climbingStairs(n) = 2.
 You can either climb 2 steps at once or climb 1 step two times.
 */
 
-
+//Function that resolve this
 function climbingStairs(steps) { 
     //Here is with Dyanmic Programming Top-Down Manner, this method is not eficient for 42 steps or up
     /*
@@ -20,8 +25,7 @@ function climbingStairs(steps) {
     return climbingStairs(steps - 1) + climbingStairs(steps - 2)
     */
 
-
-    //This is by Bottom-Up Manner
+    //This is by Bottom-Up Manner, optimized using Memoization
     if(steps == 1) return 1
     if(steps == 2) return 2
     
@@ -38,30 +42,6 @@ function climbingStairs(steps) {
     }
     return previous[1]
 }
-//Desktop Test
-/**
-n = 4
-
-previous = [1,2]
-current = 2
-2 < 4
-
-first iteration
-pretotal = previous[0] + previous[1]//1 + 2 //3
-previous[0] = previous[1] //2
-previous[1] = pretotal // 3
-current = 3
-
-second iteration
-3 < 4
-pretotal = previous[0] + previous[1]//2 + 3 //5
-previous[0] = previous[1] //3
-previous[1] = pretotal // 5
-current = 4
-
-end iteration
-
-return previous[1] // 5
-
-
+/*
+Test and Try to improve the solution by yourself :D
 */
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # DynamicProgramming
-Dynamic Programming Exercises Written in Javascript
 
-Here we will be posting all Scripts that solves a certain problem using Dynamic Programming.
+##¿What is Dynamic Programming?
+In the concept is a programming paradigm but in my learning across the DP, I think it's more a skill that We can implement in a any kind of paradigm, like POO or Functional to improve the performance of our code.
+
+## Dynamic Programming Exercises Written in Javascript
+
+Here I will be posting all Scripts that solves a certain problem using Dynamic Programming concepts.
+
 Dynamic Programming is a method to optimize solutions for problems using a several techniques or splitting up the problem into subproblems.
 Personally I think the theory for people to low level knowlegde in Maths are so hard.
 But practicing in problems all can understand little by little this very high tool for any Developer, also will be you a better Software Egineer.
+
+## ¿Why Javascript?
+Because is the language I most used today, but applies in any language is more about algorithms. I strongly recommended too use in languages that are very focused in Functional Programming like Haskell, Rust, etc. Also Kotlin is very focused in Poo and Functional.
+
+In my excercies always I will try implement latest features of javascript like destructuring and so on...
